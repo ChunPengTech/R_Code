275f694146167c99b2f71fa8d34b1d24133f45d7
ChunPengTech <1981864990@qq.com>
2025-04-14 10:53:07 +0800

    Fixed some bugs

--- 实证/面板回归/常用方法/滞后变量.R
+++ 实证/面板回归/常用方法/滞后变量.R
@@ -5,6 +5,6 @@ data <- import("data.xlsx", as = "tibble")
 
 data |>
   group_by(province) |>
-  mutate(L.x = lag(y), L.x = lag(x)) |>
+  mutate(L.y = lag(y), L.x = lag(x)) |>
   select(province, year, y, x, L.y, L.x) |>
   View()
